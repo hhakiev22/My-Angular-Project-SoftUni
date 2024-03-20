364d292e3d4f5616e6ac2685a4fa8faa06b00892
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-03-20 19:19:10 +0200

    Add search button and make the button work

--- food-app/src/app/components/components.module.ts
+++ food-app/src/app/components/components.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './partials/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RouterModule } from '@angular/router';
+import { SearchComponent } from './partials/search/search.component';
 
 @NgModule({
-  declarations: [HeaderComponent, HomeComponent],
+  declarations: [HeaderComponent, HomeComponent, SearchComponent],
   imports: [CommonModule, RouterModule],
-  exports: [HeaderComponent, HomeComponent],
+  exports: [HeaderComponent, HomeComponent, SearchComponent],
 })
 export class ComponentsModule {}
