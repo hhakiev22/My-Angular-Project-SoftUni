ba02e1a07a7452e371d51fca1ef6daa6e1cefc03
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-03-23 00:47:48 +0200

    Generate Cart service

--- food-app/src/app/service/cart.service.ts
+++ food-app/src/app/service/cart.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Cart } from '../shared/models/Cart';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
-
-  constructor() { }
+  private cart: Cart = new Cart();
+  private cartSubject: BehaviorSubject<Cart> = new BehaviorSubject(this.cart);
+  constructor() {}
 }
