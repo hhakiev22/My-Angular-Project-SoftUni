72f57db4834912df7b585e62fa2b38e2b9a649bd
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-03-23 00:51:11 +0200

    create addToCart functionality

--- food-app/src/app/service/cart.service.ts
+++ food-app/src/app/service/cart.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Cart } from '../shared/models/Cart';
 import { BehaviorSubject } from 'rxjs';
+import { Fitness } from '../shared/models/Fitness';
+import { CartItem } from '../shared/models/CartItem';
 
 @Injectable({
   providedIn: 'root',
@@ -9,4 +11,15 @@ export class CartService {
   private cart: Cart = new Cart();
   private cartSubject: BehaviorSubject<Cart> = new BehaviorSubject(this.cart);
   constructor() {}
+
+  addToCart(fitness: Fitness): void {
+    let cartItem = this.cart.items.find(
+      (item) => item.fitness.id == fitness.id
+    );
+    // we search for the item to add to the cart
+
+    if (cartItem) return;
+
+    this.cart.items.push(new CartItem(fitness));
+  }
 }
