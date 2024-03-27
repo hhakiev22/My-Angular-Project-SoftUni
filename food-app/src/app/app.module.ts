e7f72bc0c14b5ad32e0ff29d6583cee23d18ceef
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-03-27 19:15:30 +0200

    Add ngx-toastr service

--- food-app/src/app/app.module.ts
+++ food-app/src/app/app.module.ts
@@ -5,14 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     ComponentsModule,
     ReactiveFormsModule,
+    ToastrModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
