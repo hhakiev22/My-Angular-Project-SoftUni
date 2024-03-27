0f3c82e49fd5922bb6e0fddfb7b03c3ef1d30c55
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-03-27 18:37:22 +0200

    Add login subject

--- food-app/src/app/services/user.service.ts
+++ food-app/src/app/services/user.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../shared/models/User';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private userSubject = new BehaviorSubject<User>(new User());
+  public userObservable: Observable<User>;
 
-  constructor() { }
+  constructor() {
+    this.userObservable = this.userSubject.asObservable();
+  }
 }
