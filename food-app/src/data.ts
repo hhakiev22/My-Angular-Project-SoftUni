0c6737eb17388c50aef585e2a09b00cd31da78d5
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-03-28 23:25:08 +0200

    Add tags html and css

--- food-app/src/data.ts
+++ food-app/src/data.ts
@@ -49,11 +49,10 @@ export const sample_foods: Food[] = [
 ];
 
 export const sample_tags: Tag[] = [
-  { name: 'All', count: 6 },
-  { name: 'FastFood', count: 4 },
-  { name: 'Cheescake', count: 2 },
+  { name: 'All', count: 4 },
+  { name: 'FastFood', count: 3 },
   { name: 'Lunch', count: 3 },
-  { name: 'SlowFood', count: 2 },
+  { name: 'SlowFood', count: 1 },
   { name: 'Salad', count: 1 },
   { name: 'Soup', count: 1 },
 ];
