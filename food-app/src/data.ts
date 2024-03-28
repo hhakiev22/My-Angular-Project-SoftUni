8a85cf8956ba48519913f4d951f5bbb732a9694a
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-03-28 22:48:03 +0200

    Add home and generate food service

---
+++ food-app/src/data.ts
@@ -0,0 +1,48 @@
+import { Food } from './app/shared/models/Food';
+
+export const sample_foods: Food[] = [
+  {
+    id: '1',
+    name: 'Cheescake',
+    cookTime: '10-20',
+    price: 10,
+    favorite: false,
+    origins: ['italy'],
+    stars: 4.5,
+    imageUrl: 'assets/cheescake.jpg',
+    tags: ['FastFood', 'Desert', 'Lunch'],
+  },
+  {
+    id: '2',
+    name: 'Bo Tai Chanh',
+    price: 20,
+    cookTime: '20-30',
+    favorite: true,
+    origins: ['persia', 'middle east', 'china'],
+    stars: 4.7,
+    imageUrl: 'assets/boTaiChanh.jpg',
+    tags: ['SlowFood', 'Lunch'],
+  },
+  {
+    id: '3',
+    name: 'Pumpkin Soup',
+    price: 5,
+    cookTime: '10-15',
+    favorite: false,
+    origins: ['germany', 'us'],
+    stars: 3.5,
+    imageUrl: 'assets/pumpkinSoup.jpg',
+    tags: ['FastFood', 'Soup'],
+  },
+  {
+    id: '4',
+    name: 'Tofu Pad Thai',
+    price: 8,
+    cookTime: '15-20',
+    favorite: true,
+    origins: ['belgium', 'france'],
+    stars: 3.3,
+    imageUrl: 'assets/TofuPadThai.webp',
+    tags: ['FastFood', 'Lunch', 'Salad'],
+  },
+];
