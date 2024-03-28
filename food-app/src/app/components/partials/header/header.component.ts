70c48e09dd4b0ed7e7967605e53f427eee9ae124
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-03-29 00:09:45 +0200

    Add quantity to Cart button in the header

--- food-app/src/app/components/partials/header/header.component.ts
+++ food-app/src/app/components/partials/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  cartQuantity = 0;
+
+  constructor(cartService: CartService) {
+    cartService.getCartObservable().subscribe((newCart) => {
+      this.cartQuantity = newCart.totalCount;
+    });
+  }
 
   ngOnInit(): void {}
 }
