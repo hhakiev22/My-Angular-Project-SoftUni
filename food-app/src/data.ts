94d0e18151b137603d3c316b692d87ecf373f425
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-03-28 23:11:55 +0200

    Add sample tags to food ts

--- food-app/src/data.ts
+++ food-app/src/data.ts
@@ -1,4 +1,5 @@
 import { Food } from './app/shared/models/Food';
+import { Tag } from './app/shared/models/Tag';
 
 export const sample_foods: Food[] = [
   {
@@ -45,4 +46,14 @@ export const sample_foods: Food[] = [
     imageUrl: 'assets/TofuPadThai.webp',
     tags: ['FastFood', 'Lunch', 'Salad'],
   },
+];
+
+export const sample_tags: Tag[] = [
+  { name: 'All', count: 6 },
+  { name: 'FastFood', count: 4 },
+  { name: 'Cheescake', count: 2 },
+  { name: 'Lunch', count: 3 },
+  { name: 'SlowFood', count: 2 },
+  { name: 'Salad', count: 1 },
+  { name: 'Soup', count: 1 },
 ];
