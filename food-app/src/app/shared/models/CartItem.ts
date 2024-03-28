dad9daecf5282612af72d9b729067ab1b75b22c1
Haka Hakiev <122813219+hhakiev22@users.noreply.github.com>
2024-03-28 21:58:26 +0200

    Delete food-app directory

--- food-app/src/app/shared/models/CartItem.ts
+++
@@ -1,7 +0,0 @@
-import { HealthyFood } from './Food';
-
-export class CartItem {
-  constructor(public food: HealthyFood) {} // inject HealthyFood
-  quantity: number = 1;
-  price: number = this.food.price;
-}
