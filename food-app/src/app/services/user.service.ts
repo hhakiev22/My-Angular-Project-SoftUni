d4c03b67df4457747d2bc846ff5b882fc0a783e7
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-03-30 10:00:21 +0200

    Submit method

--- food-app/src/app/services/user.service.ts
+++ food-app/src/app/services/user.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { User } from '../shared/models/User';
+import { HttpClient } from '@angular/common/http';
+import { USER_LOGIN_URL } from '../shared/constants/urls';
+import { IUserLogin } from '../shared/interfaces/IUserLogin';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +13,25 @@ export class UserService {
   private userSubject = new BehaviorSubject<User>(new User());
   public userObservable: Observable<User>;
 
-  constructor() {
+  constructor(private http: HttpClient, private toastrService: ToastrService) {
     this.userObservable = this.userSubject.asObservable();
+  }
+
+  login(userLogin: IUserLogin): Observable<User> {
+    return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
+      tap({
+        next: (user) => {
+          // this.setUserToLocalStorage(user);
+          this.userSubject.next(user);
+          this.toastrService.success(
+            `Welcome to Foodmine ${user.name}!`,
+            'Login Successful'
+          );
+        },
+        error: (errorResponse) => {
+          this.toastrService.error(errorResponse.error, 'Login Failed');
+        },
+      })
+    );
   }
 }
