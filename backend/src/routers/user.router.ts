545765a54b563b299d10d8a1ff79c2a81da9b885
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-03-31 15:09:12 +0300

    Use Database instead of data.ts

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -1,22 +1,39 @@
 import { Router } from "express";
 import { sample_users } from "../data";
 import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import { UserModel } from "../models/user.model";
 
 const router = Router();
 
-router.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  const user = sample_users.find(
-    (user) => user.email === email && user.password === password
-  );
+router.get(
+  "/seed",
+  asyncHandler(async (req, res) => {
+    const usersCount = await UserModel.countDocuments();
+    if (usersCount > 0) {
+      res.send("Seed is already done!");
+      return;
+    }
 
-  if (user) {
-    res.send(generateTokenResponse(user));
-  } else {
-    const BAD_REQUEST = 400;
-    res.status(BAD_REQUEST).send("Username or password is invalid!");
-  }
-});
+    await UserModel.create(sample_users);
+    res.send("Seed Is Done!");
+  })
+);
+
+router.post(
+  "/login",
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email, password });
+
+    if (user) {
+      res.send(generateTokenResponse(user));
+    } else {
+      const BAD_REQUEST = 400;
+      res.status(BAD_REQUEST).send("Username or password is invalid!");
+    }
+  })
+);
 
 const generateTokenResponse = (user: any) => {
   const token = jwt.sign(
