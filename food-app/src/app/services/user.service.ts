bbbf8cea6bb4ccecaceb2819a7d2a3822277d117
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-03-31 23:52:15 +0300

    Adding register method to the User Service

--- food-app/src/app/services/user.service.ts
+++ food-app/src/app/services/user.service.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { User } from '../shared/models/User';
 import { HttpClient } from '@angular/common/http';
-import { USER_LOGIN_URL } from '../shared/constants/urls';
+import { USER_LOGIN_URL, USER_REGISTER_URL } from '../shared/constants/urls';
 import { IUserLogin } from '../shared/interfaces/IUserLogin';
 import { ToastrService } from 'ngx-toastr';
+import { IUserRegister } from '../shared/interfaces/IUserRegister';
 
 const USER_KEY = 'User';
 @Injectable({
@@ -33,6 +34,24 @@ export class UserService {
         },
         error: (errorResponse) => {
           this.toastrService.error(errorResponse.error, 'Login Failed');
+        },
+      })
+    );
+  }
+
+  register(userRegister: IUserRegister): Observable<User> {
+    return this.http.post<User>(USER_REGISTER_URL, userRegister).pipe(
+      tap({
+        next: (user) => {
+          this.setUserToLocalStorage(user);
+          this.userSubject.next(user);
+          this.toastrService.success(
+            `Welcome to the Food App ${user.name}`,
+            'Register Successful'
+          );
+        },
+        error: (errorResponse) => {
+          this.toastrService.error(errorResponse.error, 'Register Failed');
         },
       })
     );
