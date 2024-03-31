e656d8170f7fdbb6fbd552501baf1165e59eb87a
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-04-01 00:04:20 +0300

    Defining register form

--- food-app/src/app/components/pages/register-page/register-page.component.ts
+++ food-app/src/app/components/pages/register-page/register-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.css']
+  styleUrls: ['./register-page.component.css'],
 })
-export class RegisterPageComponent {
+export class RegisterPageComponent implements OnInit {
+  registerForm!: FormGroup;
+  isSubmitted = false;
 
+  returnUrl = '';
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
 }
