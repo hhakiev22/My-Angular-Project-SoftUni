f2ea2b38503b5055f3e43c0f6f1f93409508dd71
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-03-31 22:01:30 +0300

    Adding register Api

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -2,7 +2,9 @@ import { Router } from "express";
 import { sample_users } from "../data";
 import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
-import { UserModel } from "../models/user.model";
+import { User, UserModel } from "../models/user.model";
+import { HTTP_BAD_REQUEST } from "../constants/http_status";
+import bcrypt from "bcryptjs";
 
 const router = Router();
 
@@ -29,9 +31,34 @@ router.post(
     if (user) {
       res.send(generateTokenResponse(user));
     } else {
-      const BAD_REQUEST = 400;
-      res.status(BAD_REQUEST).send("Username or password is invalid!");
+      res.status(HTTP_BAD_REQUEST).send("Username or password is invalid!");
     }
+  })
+);
+
+router.post(
+  "/register",
+  asyncHandler(async (req, res) => {
+    const { name, email, password, address } = req.body;
+    const user = await UserModel.findOne({ email });
+    if (user) {
+      res.status(HTTP_BAD_REQUEST).send("User already exist, please login!");
+      return;
+    }
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const newUser: User = {
+      id: "",
+      name,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+      address,
+      isAdmin: false,
+    };
+
+    const dbUser = await UserModel.create(newUser);
+    res.send(generateTokenResponse(dbUser));
   })
 );
 
