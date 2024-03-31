113958d4de6909f730c9814c972ac8566b1b90d8
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-04-01 00:14:36 +0300

    Register component's submit method
    Return URL

--- food-app/src/app/components/pages/register-page/register-page.component.ts
+++ food-app/src/app/components/pages/register-page/register-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
+import { IUserRegister } from 'src/app/shared/interfaces/IUserRegister';
 import { PasswordsMatchValidator } from 'src/app/shared/validators/password_match_validator';
 
 @Component({
@@ -34,5 +35,29 @@ export class RegisterPageComponent implements OnInit {
         validators: PasswordsMatchValidator('password', 'confirmPassword'),
       }
     );
+
+    this.returnUrl = this.activatedRoute.snapshot.queryParams.returnUrl;
+  }
+
+  get fc() {
+    return this.registerForm.controls;
+  }
+
+  submit() {
+    this.isSubmitted = true;
+    if (this.registerForm.invalid) return;
+
+    const fv = this.registerForm.value;
+    const user: IUserRegister = {
+      name: fv.name,
+      email: fv.email,
+      password: fv.password,
+      confirmPassword: fv.confirmPassword,
+      address: fv.address,
+    };
+
+    this.userService.register(user).subscribe((_) => {
+      this.router.navigateByUrl(this.returnUrl);
+    });
   }
 }
