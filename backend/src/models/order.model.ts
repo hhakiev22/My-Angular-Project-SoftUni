ee00e80d177a2c4b429bea7a69d0c315408bcf44
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-04-01 22:26:18 +0300

    Adding Order Model

---
+++ backend/src/models/order.model.ts
@@ -0,0 +1,14 @@
+import { model, Schema, Types } from "mongoose";
+import { Food, FoodSchema } from "./food.model";
+
+export interface OrderItem {
+  food: Food;
+  price: number;
+  quantity: number;
+}
+
+export const OrderItemSchema = new Schema<OrderItem>({
+  food: { type: FoodSchema, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+});
